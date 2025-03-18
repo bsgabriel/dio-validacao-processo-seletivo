925ff9aef306bc941a54e9b5af61d61f78657485
bsgabriel <48328182+bsgabriel@users.noreply.github.com>
2025-03-18 11:57:55 -0300

    Realização do exercício

---
+++ src/bsg/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package bsg.exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
